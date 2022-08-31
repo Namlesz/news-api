6c60eef1bed15e80d4aa7001dec056f38e2c4942
Adam Wiśniewski <nejmlesz@gmail.com>
2022-08-31 18:21:44 +0200

    delete comment

--- news_api/Controllers/WeatherForecastController.cs
+++ news_api/Controllers/WeatherForecastController.cs
@@ -18,17 +18,6 @@ public class WeatherForecastController : ControllerBase
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
 
-    // [HttpGet(Name = "GetWeatherForecast")]
-    // public IEnumerable<WeatherForecast> Get()
-    // {
-    //     return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-    //         {
-    //             Date = DateTime.Now.AddDays(index),
-    //             TemperatureC = Random.Shared.Next(-20, 55),
-    //             Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-    //         })
-    //         .ToArray();
-    // }
     [HttpGet]
     public async Task<List<Book>> Get() =>
         await _newsService.GetAsync();
